b7c4008ba3e5dc453a17b84485f7368305a29e86
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2025-08-10 01:24:47 +0600

    Project initialization- Live object detection using CoreML

---
+++ LiveObjectDetection/LiveObjectDetection/LiveObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveObjectDetectionApp.swift
+//  LiveObjectDetection
+//
+//  Created by Md. Faysal Ahmed on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
